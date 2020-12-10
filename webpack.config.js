e10e18caa048d1fd2539225bccb4a3465b44456c
Andi Angel <andi@bitweb.ee>
2020-12-10 17:18:40 +0200

    Add hot reload to Webpack development server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  target: 'web',
   entry: './src/index.js',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
+    watchContentBase: true,
     compress: true,
     port: 3000,
+    hot: true
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
