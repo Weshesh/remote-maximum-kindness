8a9fe8a0ce9b1434d62207d4a6a41f2b2760d112
Andi Angel <andi@bitweb.ee>
2020-12-11 14:46:53 +0200

    Add TypeScript to WP

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import _ from 'lodash';
 import './styles.css';
 
 function component() {
   const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,16 @@ const path = require('path');
 
 module.exports = {
   target: 'web',
-  entry: './src/index.js',
+  entry: './src/index.ts',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     watchContentBase: true,
     compress: true,
     port: 3000,
     hot: true
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -17,6 +20,11 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
